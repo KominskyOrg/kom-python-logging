4e998bc98840e363c2e04d2f85d2b332a531522c
jaredkominsky <kominskyjared@gmail.com>
2024-08-07 21:06:34 -0500

    changing name of package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="kom_pythong_logging",
+    name="kom_python_logging",
     version="0.1",
     packages=find_packages(),
     install_requires=[],
